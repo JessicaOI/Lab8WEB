b109313096a3cbd65070135b0b3999b4284b93d5
Jessica Ortiz <70552512+JessicaOI@users.noreply.github.com>
2022-05-23 14:35:01 -0600

    fronted

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+//refereniqa de codigo: https://www.youtube.com/watch?v=CMwt6Nwjf9g&ab_channel=BetoMoedano
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "calculator-grid">
+        <div className = "output">
+          <div className = "numAnterior">123</div>
+          <div className = "numActual">456</div>
+        </div>
+        <button className = "dosEspacios">AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className = "dosEspacios">=</button>
+
+      </div>
+      
     </div>
   );
 }
